d1d3b7c068849c897b94c24356196464b8abf46e
Batanai <batiemc@gmail.com>
2024-10-16 11:35:44 +0200

    affirmations and details screen

--- components/GuidedAffirmationsGallery.tsx
+++ components/GuidedAffirmationsGallery.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Pressable, Image } from 'react-native'
 import React from 'react'
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
+import { Link } from 'expo-router';
 
 interface GuidedAffirmationsGalleryProp {
     title: string;
@@ -10,7 +11,30 @@ interface GuidedAffirmationsGalleryProp {
 const GuidedAffirmationsGallery = ({title, previews}: GuidedAffirmationsGalleryProp) => {
   return (
     <View className='my-5'>
-      <Text>GuidedAffirmationsGallery</Text>
+      <View className='mb-2'>
+        <Text className='text-white font-bold '>{title}</Text>
+      </View>
+      <View>
+        <FlatList
+          horizontal
+          data={previews}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => (
+            <Link href={`/affirmations/${item.id}`} asChild>
+                <Pressable>
+                  <View className='h-36 w-32 rounded-md mr-5'>
+                    <Image
+                      source={item.image}
+                      resizeMode='cover'
+                      className='w-full h-full'
+                    />
+                  </View>
+                </Pressable>
+            </Link>
+          )}
+        />
+      </View>
     </View>
   )
 }
